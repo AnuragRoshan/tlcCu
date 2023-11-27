1bd8d448bcb0f412458b92049386703a4f12c6df
Anurag Raushan <anuragraushan373@gmail.com>
2023-11-28 02:17:12 +0530

    updated

--- src/Page/Contactus.jsx
+++ src/Page/Contactus.jsx
@@ -10,39 +10,112 @@ const Contactus = () => {
         <div className="about-top-inner">
           <div className="about-main-top">
             <div className="about-main-inner">
-              <div className="about-element">
-                <div className="about-head">Address</div>
-                <div className="about-desc">
-                  Teaching Learning Centre, Block B3 Chandigarh University,
-                  NH-05 Chandigarh-Ludhiana Highway, Mohali, Punjab
-                  (INDIA)-140413
-                </div>
-              </div>
-              <div className="about-element">
-                <div className="about-head">Email</div>
-                <div className="about-desc">ed.tlc@cumail.in</div>
-              </div>
-              <div className="about-element">
-                <div className="about-head">Contact Number</div>
-                <div className="about-desc">
-                  {" "}
-                  <a href="tel:+" style={{ color: "blue" }}>
-                    {" "}
-                    70874-59999
-                  </a>
-                </div>
-              </div>
-              <div className="about-element">
-                <div className="about-head">YouTube Channel</div>
-                <div className="about-desc">
-                  <a
-                    href="https://www.youtube.com/@cuteachinglearningcentre4832"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ color: "blue" }}
+              <div
+                className="about-element"
+                style={{ display: "flex", width: "70%" }}
+              >
+                <div>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{
+                      fontSize: "2rem",
+                      borderRadius: "50%",
+                      padding: "0.4rem",
+                      marginInline: "0.5rem",
+                      marginBlockStart: "20%",
+                    }}
                   >
-                    https://www.youtube.com/@cuteachinglearningcentre4832
-                  </a>
+                    home
+                  </span>
+                </div>
+                <div>
+                  <div className="about-head">Address</div>
+                  <div className="about-desc" style={{ width: "100%" }}>
+                    Teaching Learning Centre, Block B3 Chandigarh University,
+                    NH-05 Chandigarh-Ludhiana Highway, Mohali, Punjab
+                    (INDIA)-140413
+                  </div>
+                </div>
+              </div>
+              <div
+                className="about-element"
+                style={{ display: "flex", width: "70%" }}
+              >
+                <div>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{
+                      fontSize: "2rem",
+                      borderRadius: "50%",
+                      padding: "0.4rem",
+                      marginInline: "0.5rem",
+                    }}
+                  >
+                    mail
+                  </span>
+                </div>
+                <div>
+                  <div className="about-head">Email</div>
+                  <div className="about-desc">ed.tlc@cumail.in</div>
+                </div>
+              </div>
+              <div
+                className="about-element"
+                style={{ display: "flex", width: "70%" }}
+              >
+                <div>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{
+                      fontSize: "2rem",
+                      borderRadius: "50%",
+                      padding: "0.4rem",
+                      marginInline: "0.5rem",
+                    }}
+                  >
+                    phone
+                  </span>
+                </div>
+                <div>
+                  <div className="about-head">Contact Number</div>
+                  <div className="about-desc">
+                    {" "}
+                    <a href="tel:+" style={{ color: "blue" }}>
+                      {" "}
+                      70874-59999
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="about-element"
+                style={{ display: "flex", width: "70%" }}
+              >
+                <div>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{
+                      fontSize: "2rem",
+                      borderRadius: "50%",
+                      padding: "0.4rem",
+                      marginInline: "0.5rem",
+                    }}
+                  >
+                    video_library
+                  </span>
+                </div>
+                <div>
+                  <div className="about-head">YouTube Channel</div>
+                  <div className="about-desc">
+                    <a
+                      href="https://www.youtube.com/@cuteachinglearningcentre4832"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ color: "blue" }}
+                    >
+                      https://www.youtube.com/@cuteachinglearningcentre4832
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -8,44 +8,24 @@ import Map from "../Components/Homepage/Map";
 const HomePage = () => {
   return (
     <>
-      {/* <div class="spinner-3"></div> */}
-      <div class="diagonal-div"></div>
-      <img
-        src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Image over red side"
-        className="image-over-red"
-      />
-      {/* <img
-        src="https://www.cuchd.in/tlc/assets/uploads/cu-logo.png"
-        className="logo-over-red"
-      ></img>
-      <img
-        src="https://www.cuchd.in/tlc/assets/uploads/naac-logo.png"
-        className="naac-logo-over-red"
-      ></img>
-      <img
-        src="https://tlccu.onrender.com/assets/uploads/TLC%20Logo%20(5).png"
-        className="tlc-logo-over-red"
-      ></img> */}
-      <div className="text-over-red">
-        <h1 className="text-over-red-head">
-          <div className="text-over-red-desc">
+      <div className="home-top">
+        <div className="home-banner">
+          <img
+            src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Image over red side"
+            className="home-banner-image"
+          />
+        </div>
+        <div className="home-banner-text">
+          <div className="banner-desc">
             FACILITATING SUSTAINED PROFESSIONAL DEVELOPMENT
           </div>
-          <div className="text-over-below">
-            <div style={{ width: "max-content" }}>
-              Teaching & <br />{" "}
-            </div>
-            <span
-              className="text-over-red-below"
-              style={{ width: "max-content" }}
-            >
-              Learning Center
-            </span>
+          <div className="banner-head">
+            <span> Teaching & </span>
+            <div style={{ color: "white" }}> Learning Center</div>
           </div>
-        </h1>
+        </div>
       </div>
-
       <div>
         <Chancelormsg />
       </div>
